fe9f5c954f309a7e79c3c1373817d6a3afe121ec
Boydem <noyemdahan@gmail.com>
2023-03-01 19:56:41 +0200

    game works!

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -2,36 +2,60 @@ import { LegacyRef, useEffect, useRef, useState } from 'react'
 
 const colors: string[] = ['green', 'red', 'yellow', 'blue']
 
+// * Sound Effects *
+const sounds: { [key: string]: HTMLAudioElement } = {
+    green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+    red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+    blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+    yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
+}
+
 export const Home = () => {
     const [level, setLevel] = useState<number>(1)
     const [isGameOn, setIsGameOn] = useState(true)
-    const randomNums = useRef<number[]>([])
+    const [isPlayerMove, setIsPlayerMove] = useState(false)
+    const sequence = useRef<number[]>([])
     const gameBtnsRefs = useRef<HTMLButtonElement[]>([])
     const timeoutIds = useRef<NodeJS.Timeout[]>([])
-    const clickCount = useRef<number>(0)
+    const playerClick = useRef<number>(0)
 
     useEffect(() => {
-        if (!isGameOn) return
-        const randomNum: number = Math.floor(Math.random() * 3) + 1
-        randomNums.current.push(randomNum)
+        document.body.focus()
+    }, [])
 
-        function hilghlightBtn(index: number) {
-            gameBtnsRefs.current[index]?.classList.add('active')
+    console.log('isPlayerMove:', isPlayerMove)
+    useEffect(() => {
+        if (!isGameOn) return
+        const randomNum: number = Math.floor(Math.random() * 3)
+        sequence.current.push(randomNum)
+
+        function hilghlightBtn(colorIdx: number, sequenceCount: number) {
+            gameBtnsRefs.current[colorIdx]?.classList.add('active')
+            playSound(colorIdx)
             timeoutIds.current.push(
                 setTimeout(() => {
-                    gameBtnsRefs.current[index]?.classList.remove('active')
-                }, 1000)
+                    gameBtnsRefs.current[colorIdx]?.classList.remove('active')
+                    console.log('colorIdx:', colorIdx)
+                    console.log('sequence.current.length - 1:', sequence.current.length - 1)
+                    if (sequenceCount === sequence.current.length - 1) {
+                        console.log('hello:')
+                        setIsPlayerMove(true)
+                    }
+                }, 250)
             )
         }
 
         for (let i = 0; i < level; i++) {
-            const timeoutId = setTimeout(() => {
-                hilghlightBtn(i)
-            }, 250 + i * 1000) // Delay each button activation by 1 second
+            const timeoutId = setTimeout(
+                () => {
+                    hilghlightBtn(sequence.current[i], i)
+                },
+                i === 0 ? 1000 : 1000 + i * 1000
+            ) // Use a longer delay for the first button activation
             timeoutIds.current.push(timeoutId)
         }
 
-        console.log('randomNums.current:', randomNums.current)
+        console.log('randomNums.current:', sequence.current)
         return () => {
             clearTimeOuts()
         }
@@ -39,13 +63,15 @@ export const Home = () => {
 
     function handleClick(clickedIdx: number): void {
         console.log('clickedIdx:', clickedIdx)
-        console.log('randomNums.current[clickCount.current]:', randomNums.current[clickCount.current])
+        console.log('randomNums.current[clickCount.current]:', sequence.current[playerClick.current])
+        playSound(clickedIdx)
         if (isCorrectMove(clickedIdx)) {
-            clickCount.current++
-            console.log('clickCount.current:', clickCount.current)
-            if (clickCount.current === level) {
+            playerClick.current++
+            console.log('clickCount.current:', playerClick.current)
+            if (playerClick.current === level) {
                 console.log('LEVEL UPPPPP**************************:')
-                clickCount.current = 0
+                playerClick.current = 0
+                setIsPlayerMove(false)
                 setLevel(prev => prev + 1)
             }
             return
@@ -53,14 +79,23 @@ export const Home = () => {
         setIsGameOn(false)
     }
 
-    const isCorrectMove = (clickedIdx: number): boolean => {
-        return randomNums.current[clickCount.current] === clickedIdx
+    function playSound(index: number) {
+        let sound = sounds[Object.keys(sounds)[index]]
+        sound.currentTime = 0
+        sound.volume = 0.1
+        sound.play()
+    }
+
+    function isCorrectMove(clickedIdx: number): boolean {
+        return sequence.current[playerClick.current] === clickedIdx
     }
 
     function resetGame() {
-        randomNums.current = []
+        playerClick.current = 0
+        sequence.current = []
         setLevel(1)
         clearTimeOuts()
+        setIsPlayerMove(false)
         setIsGameOn(true)
     }
 
@@ -75,6 +110,7 @@ export const Home = () => {
             <div className='board'>
                 {colors.map((color, idx) => (
                     <button
+                        disabled={!isPlayerMove}
                         key={idx}
                         ref={el => {
                             if (el && !gameBtnsRefs.current.includes(el)) {
@@ -86,7 +122,7 @@ export const Home = () => {
                     ></button>
                 ))}
                 <div className='level'>
-                    <h1 title='score'>{level}</h1>
+                    <h1 title='Level'>{level}</h1>
                 </div>
                 {!isGameOn && (
                     <div className='modal'>
